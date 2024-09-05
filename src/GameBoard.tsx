85720e6631a03004672f5ef00fc6c81238039862
Jimmy <jimmystry@gmail.com>
2024-09-05 10:03:19 -0400

    Minor fixes, updated words

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -53,7 +53,7 @@ export default function GameBoard() {
     }
 
     // Credit: https://stackoverflow.com/a/12646864
-    function shuffleArray(array) {
+    function shuffleArray(array: any) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
